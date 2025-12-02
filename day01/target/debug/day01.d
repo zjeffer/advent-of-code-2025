bca071aad72074c9ff4e737a479f7d59617879eb
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-12-02 13:47:50 +0100

    remove target folder

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/home/zjeffer/git/advent-of-code/day01/target/debug/day01: /home/zjeffer/git/advent-of-code/day01/data/input.txt /home/zjeffer/git/advent-of-code/day01/src/main.rs
--- day01/target/debug/deps/day01-f95dbfab787acea3.d
+++
@@ -1,6 +0,0 @@
-/home/zjeffer/git/advent-of-code/day01/target/debug/deps/day01-f95dbfab787acea3.d: src/main.rs src/../data/input.txt
-
-/home/zjeffer/git/advent-of-code/day01/target/debug/deps/day01-f95dbfab787acea3: src/main.rs src/../data/input.txt
-
-src/main.rs:
-src/../data/input.txt:
