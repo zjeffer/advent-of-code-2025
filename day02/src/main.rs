85c132a2b3dc1c115adebd787b12acf9953a3be5
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-12-14 23:36:39 +0100

    day02: part 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,30 +7,30 @@ impl Id {
         Id { value }
     }
 
-    /// A valid ID:
-    /// * Does not start with a 0
-    /// * Is not a sequence repeated twice in a row
+    /// A invalid ID is a sequence of digits repeated at least twice
     fn is_valid(&self) -> bool {
         let string_representation = self.value.to_string();
         if string_representation.len() == 1 {
             return true;
         }
 
-        if string_representation.len() % 2 != 0 {
-            return true;
+        // use the length of the sequence to gather all possible sequences
+        let max_sequence_length = string_representation.len() / 2;
+        let chars: Vec<char> = string_representation.chars().collect();
+        for length in 1..=max_sequence_length {
+            // split the string up in sequences of `length`
+            let chunks: Vec<&[char]> = chars.chunks(length).collect();
+            assert!(chunks.len() >= 2);
+            // if all chunks are the same, it's invalid
+            if chunks.iter().skip(1).all(|chunk| chunk == &chunks[0]) {
+                return false;
+            }
         }
-
-        let middle_index = string_representation.len() / 2;
-        let (first_half, second_half) = string_representation.split_at(middle_index);
-        if first_half == second_half {
-            return false;
-        }
-        return true;
+        true
     }
 }
 
 fn main() {
-    // split on ,
     let id_ranges = include_str!("../data/input.txt")
         .trim()
         .split(',')
@@ -51,7 +51,7 @@ fn main() {
         .map(|id| id.value)
         .collect();
     let invalid_sum: i64 = invalid_ids.iter().sum();
-    println!("Sum of invalid IDs: {}", invalid_sum);
+    println!("Sum of invalid IDs: {invalid_sum}");
 }
 
 #[cfg(test)]
@@ -93,7 +93,8 @@ mod tests {
         ];
 
         let expected_invalid_ids = vec![
-            11, 22, 99, 1010, 1188511885, 222222, 446446, 38593859,
+            11, 22, 99, 111, 999, 1010, 1188511885, 222222, 446446, 38593859, 565656, 824824824,
+            2121212121,
         ];
 
         let expected_sum = expected_invalid_ids.iter().sum::<i64>();
@@ -112,5 +113,30 @@ mod tests {
 
         assert_eq!(invalid_ids, expected_invalid_ids);
         assert_eq!(invalid_sum, expected_sum);
+    }
+
+    #[test]
+    fn test_full_input() {
+        let id_ranges = include_str!("../data/input.txt")
+            .trim()
+            .split(',')
+            .map(|range| {
+                let mut bounds = range.split('-').map(|s| s.parse::<i64>().unwrap());
+                let start = bounds.next().unwrap();
+                let end = bounds.next().unwrap();
+                (start, end)
+            })
+            .collect::<Vec<(i64, i64)>>();
+
+        let ids_to_check = id_ranges
+            .iter()
+            .flat_map(|(start, end)| *start..=*end)
+            .map(Id::new);
+        let invalid_ids: Vec<i64> = ids_to_check
+            .filter(|id| !id.is_valid())
+            .map(|id| id.value)
+            .collect();
+        let invalid_sum: i64 = invalid_ids.iter().sum();
+        assert_eq!(invalid_sum, 33832678380);
     }
 }
